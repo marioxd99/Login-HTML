b65f4c753fa9601bb74e058ad2797d3f5cc56d10
marioxd99 <marioxd99@gmail.com>
2020-10-27 17:31:39 +0100

    Primer commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$('.toogle').click(function(){
+    $('.formulario').animate({
+        heigth:"toogle",
+        'padding-top': 'toogle',
+        'padding-bottom': 'toogle',
+        opacity:'toogle'
+    },"slow")
+});
